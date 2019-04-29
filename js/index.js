b0500647c2996208cdb15d3310026f88e575e340
Luxor <paulo393@hotmail.com>
2019-04-28 20:51:51 -0500

    audio add

--- js/index.js
+++ js/index.js
@@ -1,22 +1,17 @@
-var i=-1;
-function delay(){
-    var array = ["#58FAF4", "#58FAD0", "#58FAAC", "#58FA82",                  "#58FA58","#82FA58","#ACFA58","#D0FA58",
-                 "#F4FA58","#F7D358","#FAAC58","#FA8258",
-                 "#FA5858","#FA5882","#FA58AC","#FA58D0",
-                 "#FA58F4","#D358F7","#AC58FA","#8258FA",
-                 "#5858FA","#2E64FE","#0080FF","#04B4AE",
-                 ,"#088A68","#58D3F7"];
-    i++;
-    setTimeout(function(){changeColor(array[i])}, 650);
+var btnPlay = document.querySelector('#playButton');
+var btnPause = document.querySelector('#pauseButton');
+
+
+window.onload = function() {
+  audio.play();
+}
+
+btnPlay.addEventListener('click', function() {
   
-    if(i===25){
-      i=-1;
-      delay();
-    }
-  }
+  audio.play();
+});
+
+btnPause.addEventListener('click', function() {
   
-  function changeColor(color){
-      $('a.email').css("color", color);
-    delay();
-   }
-delay();
+  audio.pause();
+});
